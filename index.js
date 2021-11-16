2d5bc49b4438a6f996dfad62a2e174635a784365
Roberto Jacquez <rjqz92@gmail.com>
2021-11-15 20:44:51 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+      
+    }else if(e.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+  });
+
+
+function moveDodgerRight() {
+    const leftNumbers2 = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers2, 10)
+    if (left < 360){
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+
